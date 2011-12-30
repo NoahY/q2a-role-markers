218f857b484551d62d6a5ae44e3427b2e1b39f60
NoahY <noahy57@gmail.com>
2011-12-30 08:51:35 +0530

    renamed

--- qa-marker-layer.php
+++ qa-marker-layer.php
@@ -13,10 +13,10 @@
 
 		function post_avatar($post, $class, $prefix=null)
 		{
-			if(($class == 'qa-q-view' && qa_opt('marker_plugin_a_qv')) || ($class == 'qa-q-item' && qa_opt('marker_plugin_a_qi')) || ($class == 'qa-a-item' && qa_opt('marker_plugin_a_a')) || ($class == 'qa-c-item' && qa_opt('marker_plugin_a_c'))) {
+			if(isset($post['avatar']) && (($class == 'qa-q-view' && qa_opt('marker_plugin_a_qv')) || ($class == 'qa-q-item' && qa_opt('marker_plugin_a_qi')) || ($class == 'qa-a-item' && qa_opt('marker_plugin_a_a')) || ($class == 'qa-c-item' && qa_opt('marker_plugin_a_c')))) {
 				$uid = $post['raw']['userid'];
-				$image = $this->get_avatar_marker($uid);
-				$post['avatar'] = $image.$post['avatar'];
+				$image = $this->get_role_marker($uid,1);
+				$post['avatar'] = $image.@$post['avatar'];
 			}
 			qa_html_theme_base::post_avatar($post, $class, $prefix);
 		}
@@ -25,14 +25,14 @@
 			if(isset($post['who']) && (($class == 'qa-q-view' && qa_opt('marker_plugin_w_qv')) || ($class == 'qa-q-item' && qa_opt('marker_plugin_w_qi')) || ($class == 'qa-w-item' && qa_opt('marker_plugin_w_a')) || ($class == 'qa-c-item' && qa_opt('marker_plugin_w_c')))) {
 				$handle = strip_tags($post['who']['data']);
 				$uid = $this->getuserfromhandle($handle);
-				$image = $this->get_who_marker($uid);
+				$image = $this->get_role_marker($uid,2);
 				$post['who']['data'] = $image.$post['who']['data'];
 				
 			}
 			if(isset($post['who_2']) && (($class == 'qa-q-view' && qa_opt('marker_plugin_w_qv')) || ($class == 'qa-q-item' && qa_opt('marker_plugin_w_qi')) || ($class == 'qa-w-item' && qa_opt('marker_plugin_w_a')) || ($class == 'qa-c-item' && qa_opt('marker_plugin_w_c')))) {
 				$handle = strip_tags($post['who_2']['data']);
 				$uid = $this->getuserfromhandle($handle);
-				$image = $this->get_who_marker($uid);
+				$image = $this->get_role_marker($uid,2);
 				$post['who_2']['data'] = $image.$post['who_2']['data'];
 			}
 
@@ -41,43 +41,48 @@
 		
 	// worker
 		
-		function get_avatar_marker($uid) {
-			$user = get_userdata( $uid );
-			if (isset($user->wp_capabilities['administrator'])) {
-				$level='users/level_admin';
-				$img = 'gold';
+		function get_role_marker($uid,$switch) {
+			if (QA_FINAL_EXTERNAL_USERS) {
+				$user = get_userdata( $uid );
+				if (isset($user->wp_capabilities['administrator'])) {
+					$level=qa_lang('users/level_admin');
+					$img = 'gold';
+				}
+				elseif (isset($user->wp_capabilities['editor'])) {
+					$level=qa_lang('users/level_editor');
+					$img = 'silver';
+				}
+				elseif (isset($user->wp_capabilities['contributor'])) {
+					$level=qa_lang('users/level_expert');
+					$img = 'bronze';
+				}
+				else
+					return;
+			} 
+			else {
+				$levelno = qa_db_read_one_value(
+					qa_db_query_sub(
+						'SELECT level FROM ^users WHERE userid=#',
+						$uid
+					),
+					true
+				);
+				$level = qa_user_level_string($levelno);
+				error_log($level);
+				if ($level == qa_lang('users/level_admin') || $level == qa_lang('users/level_super'))
+					$img = 'gold';
+				elseif ($level == qa_lang('users/level_editor'))
+					$img = 'silver';
+				elseif ($level == qa_lang('users/level_expert'))
+					$img = 'bronze';
+				else
+					return; 
 			}
-			elseif (isset($user->wp_capabilities['editor'])) {
-				$level='users/level_editor';
-				$img = 'silver';
-			}
-			elseif (isset($user->wp_capabilities['contributor'])) {
-				$level='users/level_expert';
-				$img = 'bronze';
-			}
-			else
-				return;
-				
-			return '<div class="qa-avatar-marker"><img title="'.qa_lang_html($level).'" width="20" src="'.QA_HTML_THEME_LAYER_URLTOROOT.$img.'.png"/></div>';
-		}
-		function get_who_marker($uid) {
-			$user = get_userdata( $uid );
-			if (isset($user->wp_capabilities['administrator'])) {
-				$level='users/level_admin';
-				$img = 'gold';
-			}
-			elseif (isset($user->wp_capabilities['editor'])) {
-				$level='users/level_editor';
-				$img = 'silver';
-			}
-			elseif (isset($user->wp_capabilities['contributor'])) {
-				$level='users/level_expert';
-				$img = 'bronze';
-			}
-			else
-				return;
-				
-			return '<span class="qa-who-marker qa-who-marker-'.$img.'" title="'.qa_lang_html($level).'">'.qa_opt('marker_plugin_who_text').'</span>';
+
+			if($switch == 1)
+				return '<div class="qa-avatar-marker"><img title="'.qa_html($level).'" width="20" src="'.QA_HTML_THEME_LAYER_URLTOROOT.$img.'.png"/></div>';
+			else 
+				return '<span class="qa-who-marker qa-who-marker-'.$img.'" title="'.qa_html($level).'">'.qa_opt('marker_plugin_who_text').'</span>';
 		}
 		function getuserfromhandle($handle) {
 			require_once QA_INCLUDE_DIR.'qa-app-users.php';
--- qa-plugin.php
+++ qa-plugin.php
@@ -18,9 +18,9 @@
 			exit;
 	}
 	
-	qa_register_plugin_layer('qa-layer.php', 'Marker Layer');	
+	qa_register_plugin_layer('qa-marker-layer.php', 'Marker Layer');	
 	
-	qa_register_plugin_module('module', 'qa-admin.php', 'qa_marker_admin', 'Role Markers');
+	qa_register_plugin_module('module', 'qa-marker-admin.php', 'qa_marker_admin', 'Role Markers');
 
 /*
 	Omit PHP closing tag to help avoid accidental output
